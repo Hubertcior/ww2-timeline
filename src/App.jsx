7e50b83edae915e440df9f8810cc1149ed9bfaf9
Hubertcior <hradziwinski@gmail.com>
2025-05-07 15:12:11 +0200

    first attemp of creating button

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
 import TimeLine from "./components/Timeline.jsx";
 import Info from "./components/Info.jsx";
+import TimelineButton from "./components/TimelineButton.jsx";
 import { events } from "./data/events.js";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   const [selectedEvent, setSelectedEvent] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleEventClick = (event) => {
     setSelectedEvent(event);
   };
 
+  const handleTimeLineButtonClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <div className="flex justify-center p-20 py-10 bg-gray-100 min-h-screen opacity-90">
+    <div className="flex justify-center py-20 bg-gray-100/90 min-h-screen ">
       <TimeLine
         events={events}
         handleClick={handleEventClick}
         selectedEvent={selectedEvent}
       />
       <Info selectedEvent={selectedEvent} />
+      <TimelineButton
+        isOpen={isOpen}
+        handleTimelineButtonClick={handleTimeLineButtonClick}
+      />
     </div>
   );
 }
--- src/components/EventIcon.jsx
+++ src/components/EventIcon.jsx
@@ -7,10 +7,13 @@ import {
   ScrollText,
   Shield,
   BicepsFlexed,
+  ArrowRightFromLine,
+  ArrowLeftFromLine,
 } from "lucide-react";
 
 const EventIcon = ({ type }) => {
   const iconClass = "text-amber-900 w-4 h-4 inline-block";
+  const iconClassForButton = "text-white w-8 h-8 inline-block opacity-100";
 
   switch (type) {
     case "start":
@@ -27,6 +30,10 @@ const EventIcon = ({ type }) => {
       return <Shield className={iconClass} />;
     case "fight":
       return <BicepsFlexed className={iconClass} />;
+    case "openButton":
+      return <ArrowRightFromLine className={iconClassForButton} />;
+    case "closeButton":
+      return <ArrowLeftFromLine className={iconClassForButton} />;
     default:
       return null;
   }
