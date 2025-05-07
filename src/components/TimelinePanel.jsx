b432b66269122b60bd04802aae0560daa2406e66
Hubert <hradziwinski@gmail.com>
2025-05-08 00:27:54 +0200

    i've styled button component for panel :)

--- src/components/TimelinePanel.jsx
+++ src/components/TimelinePanel.jsx
@@ -1,7 +1,10 @@
-const TimelinePanel = ({ children }) => {
+import PanelButton from "./PanelButton.jsx";
+
+const TimelinePanel = ({}) => {
   return (
-    <div className="fixed border-y-amber-900 border-3 right-0 mr-6 top-1/2 transform -translate-y-1/2 w-1/5 h-4/5 bg-gray-300/90 z-50 rounded-2xl flex items-center justify-center">
-      {children}
+    <div className="fixed flex-col border-y-amber-900 border-3 right-0 mr-6 top-1/2 transform -translate-y-1/2 w-1/5 h-4/5 bg-gray-300/90 z-50 rounded-2xl flex items-center justify-center">
+      <PanelButton label="Losowa data" />
+      <PanelButton label="Dzisiejsza data" />
     </div>
   );
 };
