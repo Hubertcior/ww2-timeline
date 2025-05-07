baa030bc82b6a7c74d5fb2ebc9fe387b996f0976
Hubert <hradziwinski@gmail.com>
2025-05-07 22:55:58 +0200

    styling for panel

--- src/components/TimelinePanel.jsx
+++ src/components/TimelinePanel.jsx
@@ -1,9 +1,9 @@
-const TimelinePanel = () => {
+const TimelinePanel = ({ children }) => {
   return (
-    <div className="fixed right-0 top-1/2 transform -translate-y-1/2 w-1/8 h-4/5 bg-gray-900/90 z-50 flex items-center justify-center"></div>
+    <div className="fixed border-y-amber-900 border-3 right-0 mr-6 top-1/2 transform -translate-y-1/2 w-1/5 h-4/5 bg-gray-300/90 z-50 rounded-2xl flex items-center justify-center">
+      {children}
+    </div>
   );
 };
 
 export default TimelinePanel;
-
-//TODO - better styles
