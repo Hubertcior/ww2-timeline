84ad4fadff7ba66b0d5cb72790c3b4cca475731f
Hubertcior <hradziwinski@gmail.com>
2025-05-07 16:34:07 +0200

    tooltip for button

--- src/components/TimelineButton.jsx
+++ src/components/TimelineButton.jsx
@@ -1,17 +1,24 @@
 import EventIcon from "./EventIcon.jsx";
 import { motion } from "framer-motion";
+import { Tooltip } from "react-tooltip";
 
 const TimelineButton = ({ isOpen, handleTimelineButtonClick }) => {
   return (
-    <motion.div
-      onClick={handleTimelineButtonClick}
-      initial={{ rotate: 0 }}
-      animate={{ rotate: !isOpen ? -180 : 0 }}
-      transition={{ duration: 0.2, ease: "easeInOut" }}
-      className="sticky top-1/2 left-auto right-0 transform -translate-y-1/2  rounded-full bg-amber-900 w-16 h-16 flex items-center justify-center shadow-lg cursor-pointer z-20 hover:scale-120 transition-transform duration-300 ease-in-out"
-    >
-      <EventIcon type={"openButton"} />
-    </motion.div>
+    <>
+      <Tooltip id="my-tooltip" />
+      <motion.div
+        data-tooltip-id="my-tooltip"
+        data-tooltip-content={isOpen ? "Close Timeline" : "Open Timeline"}
+        data-tooltip-variant="success"
+        onClick={handleTimelineButtonClick}
+        initial={{ rotate: 0 }}
+        animate={{ rotate: !isOpen ? -180 : 0 }}
+        transition={{ duration: 0.2, ease: "easeInOut" }}
+        className="sticky top-1/2 left-auto right-0 transform -translate-y-1/2  rounded-full bg-amber-900 w-16 h-16 flex items-center justify-center shadow-lg cursor-pointer z-20 hover:scale-120 transition-transform duration-300 ease-in-out"
+      >
+        <EventIcon type={"openButton"} />
+      </motion.div>
+    </>
   );
 };
 
